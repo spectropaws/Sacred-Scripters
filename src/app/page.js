9d1be68ff4bbbd017db293a79e5e602dedd5a9c9
SpectropawsX <spectropaws777@gmail.com>
2024-03-04 15:14:23 +0530

    restructed

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header"
 import Carousel from "./components/Carousel";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (<>
+    <Navbar />
+    <Header />
     <Carousel />
     <Footer />
   </>
