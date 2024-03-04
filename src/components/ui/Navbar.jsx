0ab462bc3689218362a1308e183cfcf9c83d45c5
mehul515 <mehulkohad@gmail.com>
2024-03-04 15:04:18 +0530

    added navbar and Header

---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { React } from "react";
+import SkewButton from "./SkewButton";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-violet-950 h-24 flex justify-between text-white font-bold">
+      <div className="flex ml-10 content-center">
+        <img className="w-14 my-auto h-14 content-center" src="/Assets/logo.png" alt="Gamerly" />
+        <h3 className="m-auto">
+          <Link href="#">GAMERLY</Link>
+        </h3>
+      </div>
+      <div className="my-auto">
+        <ul className="flex justify-center">
+          <li className="ml-11">
+            <Link href="/">HOME</Link>
+          </li>
+          <li className="ml-11">
+            <Link href="/feature">FEATURE</Link>
+          </li>
+          <li className="ml-11">
+            <Link href="/games">GAMES</Link>
+          </li>
+          <li className="ml-11">
+            <Link href="/comunity">COMMUNITY</Link>
+          </li>
+        </ul>
+      </div>
+      <SkewButton className={"w-44 mr-10 h-14 my-auto"}>PLAY NOW</SkewButton>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/ui/Slider.jsx
@@ -0,0 +1,125 @@
+"use client"
+
+import React from 'react'
+import "@/components/Slider.css"
+import { InView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+const row1 = [
+    "/Assets/scroll1.jpg",
+    "/Assets/scroll2.jpg",
+    "/Assets/scroll3.jpg",
+    "/Assets/scroll4.jpg",
+    "/Assets/scroll5.jpg",
+    "/Assets/scroll6.jpg",
+    "/Assets/scroll1.jpg",
+    "/Assets/scroll2.jpg",
+    "/Assets/scroll3.jpg",
+    "/Assets/scroll4.jpg",
+    "/Assets/scroll5.jpg",
+    "/Assets/scroll6.jpg",
+    "/Assets/scroll1.jpg",
+    "/Assets/scroll2.jpg",
+    "/Assets/scroll3.jpg",
+]
+const row2 = [
+    "/Assets/scroll7.jpg",
+    "/Assets/scroll8.jpg",
+    "/Assets/scroll9.jpg",
+    "/Assets/scroll10.jpg",
+    "/Assets/scroll11.jpg",
+    "/Assets/scroll12.jpg",
+    "/Assets/scroll13.jpg",
+    "/Assets/scroll7.jpg",
+    "/Assets/scroll8.jpg",
+    "/Assets/scroll9.jpg",
+    "/Assets/scroll10.jpg",
+    "/Assets/scroll11.jpg",
+    "/Assets/scroll12.jpg",
+    "/Assets/scroll13.jpg",
+    "/Assets/scroll7.jpg",
+]
+const row3 = [
+    "/Assets/scroll14.jpg",
+    "/Assets/scroll15.jpg",
+    "/Assets/scroll16.jpg",
+    "/Assets/scroll17.jpg",
+    "/Assets/scroll18.jpg",
+    "/Assets/scroll19.jpg",
+    "/Assets/scroll14.jpg",
+    "/Assets/scroll15.jpg",
+    "/Assets/scroll16.jpg",
+    "/Assets/scroll17.jpg",
+    "/Assets/scroll18.jpg",
+    "/Assets/scroll19.jpg",
+    "/Assets/scroll14.jpg",
+    "/Assets/scroll15.jpg",
+    "/Assets/scroll16.jpg",
+]
+
+const Slider = () => {
+
+    const containerVariants = {
+        hidden: { opacity: 0, y: 100 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.9, delay: 0.1 } }
+    };
+
+    return (
+        <>
+            <InView triggerOnce>
+                {({ inView, ref }) => (
+                    <motion.div
+                        ref={ref}
+                        variants={containerVariants}
+                        initial="hidden"
+                        animate={inView ? "visible" : "hidden"}
+                        className='relative'
+                    >
+                        <div className='slider opacity-[40%]'>
+                            <div className='slide-track'>
+                                {row1.map((img, idx) => {
+                                    return (
+                                        <div className='slide' key={idx}>
+                                            <Image width={500} height={500} className='object-cover h-44 w-80' src={img} />
+                                        </div>
+                                    )
+                                })}
+
+                            </div>
+
+                            <div className='slide-track-reverse'>
+                                {row2.map((img, idx) => {
+                                    return (
+                                        <div className='slide' key={idx}>
+                                            <Image width={500} height={500} className='object-cover h-44 w-80' src={img}/>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+
+                            <div className='slide-track'>
+                                {row3.map((img, idx) => {
+                                    return (
+                                        <div className='slide' key={idx}>
+                                            <Image width={500} height={500} className='object-cover h-44 w-80' src={img} />
+                                        </div>
+                                    )
+                                })}
+                            </div>
+
+                        </div>
+                        <div className='flex flex-col h-[60%] justify-center align-middle absolute w-[80%] top-[20%] left-[10%]' >
+                            <div className=' flex flex-col my-auto mx-auto p-10 text-center'>
+                                <p className=' text-5xl font-semibold leading-tight'>GATHER YOUR <span className=' text-purple-800'>GEARS</span> AND BUCKLE YOUR SEAT TO GET <span className=' text-purple-800'>STORMING EXPERIENCE!!</span></p>
+                                <p className='px-[20%] mt-3 text-center leading-5 font-medium text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas at vel eaque temporibus dicta perferendis repellendus, corrupti vero maxime quam minima laudantium voluptatum totam ex optio porro libero suscipit</p>
+                            </div>
+                        </div>
+                    </motion.div>
+                )}
+            </InView>
+        </>
+    )
+}
+
+export default Slider
