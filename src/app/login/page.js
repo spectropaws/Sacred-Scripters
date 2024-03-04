aa76129f7c95b988c351643f26c7b683d3cd2602
Suyash Borikar <119875820+Suyash2304@users.noreply.github.com>
2024-03-04 18:12:29 +0530

    added login and signup

---
+++ src/app/login/page.js
@@ -0,0 +1,129 @@
+"use client";
+
+import SkewButton from "@/components/SkewButton";
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { InView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
+
+export const Login = () => {
+  const navigate = useRouter();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleFormChange(event) {
+    setFormData((currData) => {
+      currData[event.target.name] = event.target.value;
+      return { ...currData };
+    });
+  }
+
+  async function handleFormSubmit(event) {
+    // event.preventDefault();
+    try {
+      console.log(formData);
+      var response = await axios.post(
+        "http://localhost:8080/user/login",
+        formData
+      );
+      // console.log(response.data, "   ",);
+
+      if (response.status === 200) {
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("name", response.data.name);
+        toast.success(response.data.message);
+        setTimeout(() => {
+          navigate.push("/LayoutTest");
+        }, 1000);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+      // console.log(error);
+    }
+    setFormData({
+      email: "",
+      password: "",
+    });
+  }
+
+  const containerVariants = {
+    hidden: { opacity: 0, y: 100 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.9, delay: 0.6 } },
+  };
+
+  return (
+    <InView triggerOnce>
+      {({ inView, ref }) => (
+        <div className="relative h-[600px] w-[1100px] rounded-[40px] mx-auto overflow-clip  bg-blue-950 ">
+          <motion.div
+            ref={ref}
+            variants={containerVariants}
+            initial="hidden"
+            animate={inView ? "visible" : "hidden"}
+          >
+            <div className="h-[100px] -ms-20 rounded-full rotate-[60deg] w-[380px] bg-slate-400 absolute top-0 left-[230px]"></div>
+            <div className="h-[280px] -ms-20 rounded-full rotate-[60deg] w-[390px] bg-slate-400 absolute top-0 left-0"></div>
+            <div className="h-[100px] -ms-20 rounded-full rotate-[60deg] w-[300px] bg-slate-400 absolute top-[350px] -left-[70px]"></div>
+          </motion.div>
+          <div className="h-[280px] -ms-20 rounded-full rotate-[60deg] w-[500px] bg-slate-400 absolute -bottom-[120px] left-[220px]"></div>
+          <div className="h-full flex justify-center absolute top-0 right-0 w-2/4">
+            <div className=" w-full text-center my-auto">
+              <h1 className="text-center text-4xl font-[700] mb-5">
+                USER LOGIN
+              </h1>
+
+              <form action={handleFormSubmit}>
+                <div>
+                  <input
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    value={formData.name}
+                    onChange={handleFormChange}
+                    required
+                    className=" px-10 h-9 bg-black w-80 placeholder-slate-50 font-medium text-[18px] rounded-md my-2"
+                  />
+                  <br />
+                  <input
+                    type="text"
+                    placeholder="Password"
+                    name="password"
+                    value={formData.name}
+                    onChange={handleFormChange}
+                    required
+                    className=" px-10 h-9 bg-black w-80 placeholder-slate-50 font-medium text-[18px] rounded-md my-2"
+                  />
+                </div>
+                <div className="mt-6">
+                  <SkewButton className={"h-10 pt-1.5 w-32 "}>
+                    {" "}
+                    LOGIN{" "}
+                  </SkewButton>
+                </div>
+              </form>
+
+              <div className="divide-blue-600 mt-3">
+                <Link href="#">Forgot Password?</Link>
+                <hr className="mt-4 w-[400px] mx-auto" />
+              </div>
+              <div className="divide-blue-600 mt-3">
+                <p href="#">Don't have an account</p>
+                <SkewButton className={"h-10 pt-1.5 px-2 w-56 mt-2"}>
+                  {" "}
+                  CREATE ACCOUNT{" "}
+                </SkewButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </InView>
+  );
+};
