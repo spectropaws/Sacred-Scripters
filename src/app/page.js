7ad054c7d05d750873b92b5abb848062611e8a6d
SpectropawsX <spectropaws777@gmail.com>
2024-03-04 17:42:30 +0530

    removed suspense

--- src/app/page.js
+++ src/app/page.js
@@ -5,27 +5,16 @@ import Navbar from "./components/Navbar";
 import StreamNow from "./components/StreamNow";
 import CollectTokens from "./components/CollectTokens";
 import TopFiveGames from "./components/TopFiveGames";
-import { Suspense } from "react";
-import Loading from "./loading";
-
-async function App() {
-  return (
-    <>
-      <Navbar />
-      <Header />
-      <Carousel />
-      <CollectTokens />
-      <StreamNow />
-      <TopFiveGames />
-      <Footer />
-    </>
-  )
-}
 
 export default function Home() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <App />
-    </Suspense>
+  return (<>
+    <Navbar />
+    <Header />
+    <Carousel />
+    <CollectTokens />
+    <StreamNow />
+    <TopFiveGames />
+    <Footer />
+  </>
   );
 }
