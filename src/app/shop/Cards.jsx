91e57778e58709001b2e42506f9ec708b51e9903
supremeStrange-04 <supreme.strange.199@gmail.com>
2024-03-04 18:02:17 +0530

    Added Shop, Tokens and Cards

---
+++ src/app/shop/Cards.jsx
@@ -0,0 +1,22 @@
+function Cards(props) {
+    return (
+      <div className="rounded-2xl min-w-[240px] h-[280px] mr-7 bg-[#150050]">
+        <img
+          className="min-w-[240px] h-[125px] rounded-t-2xl"
+          src={props.icon}
+          alt="Game Icon"
+        />
+        <h3 className="text-[#885fff] mt-1 text-xs px-3">{props.genre}</h3>
+        <h6 className="text-[#ffff] text-white text-lg font-semibold px-3">
+          {props.gameName}
+        </h6>
+        <p className="text-[#ffff] px-3 text-sm">{props.description}</p>
+        <button className="text-[#885fff] text-xs font-semibold px-3 py-2">
+          PLAY NOW
+        </button>
+      </div>
+    );
+  }
+  
+  export default Cards;
+  
---
+++ src/app/shop/Tokens.jsx
@@ -0,0 +1,19 @@
+import SkewButton from "./SkewButton";
+
+function Tokens(props) {
+  return (
+    <div className="p-4 w-[90%] h-auto bg-[#150050] rounded-2xl mb-3 shadow-lg shadow-black">
+      <h5 className="text-[#CFAA27] font-semibold">{props.gameName}</h5>
+      <h3 className="font-semibold text-white">{props.tokens} Tokens</h3>
+      <div className="flex justify-between">
+        <h5 className="text-[#CFAA27] font-semibold my-2">
+          ₹ <span className="text-2xl">{props.price}</span>
+        </h5>
+        <button>
+          <span className="text-white mx-5 px-5">BUY NOW</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Tokens;
