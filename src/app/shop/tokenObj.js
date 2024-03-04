9387a054a88c379253b6a64627e778ab87886291
supremeStrange-04 <supreme.strange.199@gmail.com>
2024-03-04 18:05:21 +0530

    Added Temporary Json files

---
+++ src/app/shop/tokenObj.js
@@ -0,0 +1,22 @@
+const TokenData = [
+  {
+    id: 1,
+    gameName: "GOD OF WAR",
+    tokens: 80,
+    price: 1200,
+  },
+  {
+    id: 2,
+    gameName: "GOD OF WAR",
+    tokens: 80,
+    price: 1200,
+  },
+  {
+    id: 3,
+    gameName: "GOD OF WAR",
+    tokens: 80,
+    price: 1200,
+  }
+];
+
+export default TokenData;
