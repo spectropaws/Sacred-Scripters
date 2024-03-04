b002caac4b9e9f248586efbebba70e4e6965334f
SpectropawsX <spectropaws777@gmail.com>
2024-03-04 17:34:21 +0530

    testing suspense and added 2 more routes

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,27 @@ import Navbar from "./components/Navbar";
 import StreamNow from "./components/StreamNow";
 import CollectTokens from "./components/CollectTokens";
 import TopFiveGames from "./components/TopFiveGames";
+import { Suspense } from "react";
+import Loading from "./loading";
+
+async function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Carousel />
+      <CollectTokens />
+      <StreamNow />
+      <TopFiveGames />
+      <Footer />
+    </>
+  )
+}
 
 export default function Home() {
-  return (<>
-    <Navbar />
-    <Header />
-    <Carousel />
-    <CollectTokens />
-    <StreamNow />
-    <TopFiveGames />
-    <Footer />
-  </>
+  return (
+    <Suspense fallback={<Loading />}>
+      <App />
+    </Suspense>
   );
 }
